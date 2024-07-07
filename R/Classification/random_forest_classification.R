8e55970956985959f8b6931ffc03d4b2ad612813
EvolveNextGen <noble@evolvengen.com>
2024-07-07 00:38:10 -0400

    Add files via upload

---
+++ R/Classification/random_forest_classification.R
@@ -0,0 +1,46 @@
+# Random Forest Classification
+
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
+
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+
+# Fitting Random Forest Classification to the Training set
+# install.packages('randomForest')
+library(randomForest)
+set.seed(123)
+classifier = randomForest(x = training_set[-3],
+                          y = training_set$Purchased,
+                          ntree = 500)
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+library(caret)
+predicted_values <- predict(classifier, test_set,type= "prob")
+head(predicted_values)
+
+threshold <- 0.5
+pred <- factor( ifelse(predicted_values[,2] > threshold, 1, 0) )
+levels(test_set$Purchased)[2]
+
+
+confusionMatrix(pred, test_set$Purchased, 
+                positive = levels(test_set$Purchased)[2])
